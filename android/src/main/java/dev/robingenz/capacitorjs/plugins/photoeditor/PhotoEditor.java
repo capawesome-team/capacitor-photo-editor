9a28b3aacd04f0a308daa94e5e6e01d9a981d08e
Robin Genz <mail@robingenz.dev>
2021-09-19 11:22:58 +0200

    wip: android

--- android/src/main/java/dev/robingenz/capacitorjs/plugins/photoeditor/PhotoEditor.java
+++ android/src/main/java/dev/robingenz/capacitorjs/plugins/photoeditor/PhotoEditor.java
@@ -1,8 +1,48 @@
 package dev.robingenz.capacitorjs.plugins.photoeditor;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
+import android.provider.MediaStore;
+
+import androidx.core.content.FileProvider;
+
+import com.getcapacitor.Bridge;
+import com.getcapacitor.Logger;
+
+import java.io.File;
+import java.util.List;
+
 public class PhotoEditor {
 
-    public String echo(String value) {
-        return value;
+    private Bridge bridge;
+
+    PhotoEditor(Bridge bridge) {
+        this.bridge = bridge;
+    }
+
+    public Intent createEditPhotoIntent(String path) {
+        try {
+            File file = new File(path);
+            String contextPackageName = bridge.getContext().getPackageName();
+            Uri uri = FileProvider.getUriForFile(bridge.getActivity(),  contextPackageName + ".fileprovider", file);
+            Intent intent = new Intent(Intent.ACTION_EDIT);
+            intent.setDataAndType(uri, "image/*");
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+            int flags = Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION;
+            intent.addFlags(flags);
+            List<ResolveInfo> resolveInfoList = bridge.getContext()
+                    .getPackageManager()
+                    .queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+            for (ResolveInfo resolveInfo : resolveInfoList) {
+                String packageName = resolveInfo.activityInfo.packageName;
+                bridge.getContext().grantUriPermission(packageName, uri, flags);
+            }
+            return intent;
+        } catch (Exception exception) {
+            Logger.error("createEditPhotoIntent failed.", exception);
+            return null;
+        }
     }
 }
