6843b7e042a20e02ab50d7ad910b19a9619ed7fc
Robin Genz <mail@robingenz.dev>
2021-09-21 20:13:39 +0200

    feat: implement `android` (#2)

--- android/src/main/java/dev/robingenz/capacitorjs/plugins/photoeditor/PhotoEditorPlugin.java
+++ android/src/main/java/dev/robingenz/capacitorjs/plugins/photoeditor/PhotoEditorPlugin.java
@@ -1,22 +1,43 @@
 package dev.robingenz.capacitorjs.plugins.photoeditor;
 
-import com.getcapacitor.JSObject;
+import android.content.Intent;
+import androidx.activity.result.ActivityResult;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
+import com.getcapacitor.annotation.ActivityCallback;
 import com.getcapacitor.annotation.CapacitorPlugin;
 
 @CapacitorPlugin(name = "PhotoEditor")
 public class PhotoEditorPlugin extends Plugin {
 
-    private PhotoEditor implementation = new PhotoEditor();
+    public static final String ERROR_PATH_MISSING = "path must be provided.";
+    public static final String ERROR_INTENT_NULL = "editPhoto failed.";
+
+    private PhotoEditor implementation;
+
+    @Override
+    public void load() {
+        implementation = new PhotoEditor(getBridge());
+    }
 
     @PluginMethod
-    public void echo(PluginCall call) {
-        String value = call.getString("value");
+    public void editPhoto(PluginCall call) {
+        String path = call.getString("path");
+        if (path == null) {
+            call.reject(ERROR_PATH_MISSING);
+            return;
+        }
+        Intent intent = implementation.createEditPhotoIntent(path);
+        if (intent == null) {
+            call.reject(ERROR_INTENT_NULL);
+            return;
+        }
+        startActivityForResult(call, intent, "handleEditPhotoResult");
+    }
 
-        JSObject ret = new JSObject();
-        ret.put("value", implementation.echo(value));
-        call.resolve(ret);
+    @ActivityCallback
+    private void handleEditPhotoResult(PluginCall call, ActivityResult result) {
+        call.resolve();
     }
 }
